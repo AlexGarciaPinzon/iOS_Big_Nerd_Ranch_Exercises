610a1349017b7cd53966695b705f056be4f27fdc
Gabriel Alexandro Garcia Pinzon <gabrielalexandro.garciapinzon@gmail.com>
2022-06-18 12:45:50 -0500

    Handling view hierarchy example
    Example of handling view hierarchy.

--- WorldTrotter/WorldTrotter/ViewController.swift
+++ WorldTrotter/WorldTrotter/ViewController.swift
@@ -12,7 +12,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //Example of handling view hierarchy
+        /*let firstFrame =  CGRect(x: 160, y:240, width: 100, height: 150)
+        let firstView = UIView(frame: firstFrame)
+        firstView.backgroundColor = UIColor.blue
+        view.addSubview(firstView)
+        
+        let secondFrame =  CGRect(x: 20, y:30, width:50, height: 50)
+        let secondView = UIView(frame: secondFrame)
+        secondView.backgroundColor = UIColor.green
+        //Original view hierarchy, green and blue views from Viewcontroller's view
+        //view.addSubview(secondView)
+        //With this change now the green view is under blue view.
+        firstView.addSubview(secondView)
+        */
     }
+     
 
 
 }
